1142d66b5083159c48574ecf9284109981975e3b
yolomep <35880616+yolomep@users.noreply.github.com>
2020-09-06 21:54:56 -0400

    author update
    and a bit more text

--- URPSS/src/playersettings/package-info.java
+++ URPSS/src/playersettings/package-info.java
@@ -1,9 +1,12 @@
 /**
  * ok so what do we need for a player?
  * hp, xp, MoveList etc. 
+ * this is a place to plan player
  */
 /**
- * @author admin
  *
+ * @author Yolomep
+ * if you spin off this game, please give credit to original author
+ * even if this code is pretty ugly
  */
 package playersettings;
